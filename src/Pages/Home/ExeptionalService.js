ca39888a1b9ada9fcd24a4688495f3e59dca7f9d
arburhan <buthan190629@outlook.com>
2022-05-11 20:23:25 +0600

    style add on exceptional section

--- src/Pages/Home/ExeptionalService.js
+++ src/Pages/Home/ExeptionalService.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import treatmentImage from '../../assets/images/treatment.png';
 
 const ExeptionalService = () => {
     return (
         <div>
-            <div class="hero min-h-screen bg-base-200">
+            <div class="hero min-h-screen">
                 <div class="hero-content flex-col lg:flex-row">
-                    <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 class="text-5xl font-bold">Box Office News!</h1>
-                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button class="btn btn-primary">Get Started</button>
+                    <img className='w-[322px] md:w-[458px] rounded-lg' src={treatmentImage} />
+                    <div className='md:px-20'>
+                        <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                        <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                        <button class="btn btn-primary text-white font-bold">Get Started</button>
                     </div>
                 </div>
             </div>
