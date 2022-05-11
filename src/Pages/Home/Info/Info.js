993284eed49e55cd231e57d771533794faeb76b1
arburhan <buthan190629@outlook.com>
2022-05-11 12:28:20 +0600

    style on card

--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -7,12 +7,10 @@ import phoneIcon from '../../../assets/icons/phone.svg';
 const Info = () => {
 
     return (
-        <div className='container mx-auto px-5'>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 text-white'>
-                <InfoCard title="Opening Hours" description="kokhon khuli ar kokhon khuli thik nai. hature daktar hami bapu :)." infoBGClass="bg-gradient-to-r from-secondary to-primary" image={clockIcon}></InfoCard>
-                <InfoCard title="Visiting our location" description="Naogaon-4, Rajshahi, Bangladesh" infoBGClass="bg-accent" image={markerIcon}></InfoCard>
-                <InfoCard title="Contact us now" description="+8801737509339" infoBGClass="bg-gradient-to-r from-secondary to-primary" image={phoneIcon}></InfoCard>
-            </div>
+        <div className='md:px-12 px-5 grid grid-cols-1 lg:grid-cols-3 gap-4 text-white'>
+            <InfoCard title="Opening Hours" description="kokhon khuli na khuli thik nai bapu :)." infoBGClass="bg-gradient-to-r from-secondary to-primary" image={clockIcon}></InfoCard>
+            <InfoCard title="Visiting our location" description="Naogaon-4, Rajshahi, Bangladesh" infoBGClass="bg-accent" image={markerIcon}></InfoCard>
+            <InfoCard title="Contact us now" description="+8801737509339" infoBGClass="bg-gradient-to-r from-secondary to-primary" image={phoneIcon}></InfoCard>
         </div>
     );
 };
