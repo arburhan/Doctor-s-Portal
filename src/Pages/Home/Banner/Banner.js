f1c774f7ae93bdf2a077071f9841314962221b8a
arburhan <buthan190629@outlook.com>
2022-05-11 21:08:24 +0600

    add custom btn & appoinment section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import chair from '../../../assets/images/chair.png';
+import PrimaryButton from '../../Shared/PrimaryButton';
 import './Banner.css';
 
 const Banner = () => {
@@ -10,7 +11,7 @@ const Banner = () => {
                 <div className=''>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary drop-shadow-[0px 4px 4px rgba(0, 0, 0, 0.25)]">GET STARTED</button>
+                    <PrimaryButton>GET STARTED</PrimaryButton>
                 </div>
             </div>
         </div>
---
+++ src/Pages/Home/HomeAppoinment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeAppoinment = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HomeAppoinment;
