da6eca9917c6e9dbf1f01e8c3de3a4fb43349b1a
arburhan <buthan190629@outlook.com>
2022-05-11 23:07:56 +0600

    appointment section added

--- src/Pages/Home/HomeAppoinment.js
+++ src/Pages/Home/HomeAppoinment.js
@@ -5,14 +5,20 @@ import PrimaryButton from '../Shared/PrimaryButton';
 
 const HomeAppoinment = () => {
     return (
-        <div className='flex justify-center items-center'>
-            <div className='flex-1 w-[606px]'>
-                <img src={doctorImage} alt="" />
+        <div style={{
+            height: '533px',
+            background: `url(${appoinmentImage}) `,
+            backgroundRepeat: 'no-repeat',
+        }} className='flex justify-center items-center my-32'>
+            <div className='flex-1 hidden lg:block'>
+                <img className='mt-[-117px]' src={doctorImage} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 px-6'>
                 <p className='text-primary font-bold my-2'>Appoinment</p>
-                <p className='my-2 text-[36px]'>Make an appointment Today</p>
-                <p className='my-6'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <div className='text-white my-5'>
+                    <p className='my-2 text-[36px]'>Make an appointment Today</p>
+                    <p className=''>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                </div>
                 <PrimaryButton>Get STarted</PrimaryButton>
             </div>
         </div>
