79d304ef6e56d1340954250631058b0e924b4a2c
arburhan <buthan190629@outlook.com>
2022-05-11 22:23:00 +0600

    appoinment section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import ExeptionalService from './ExeptionalService';
+import HomeAppoinment from './HomeAppoinment';
 import Info from './Info/Info';
 import Services from './Services';
 
@@ -11,6 +12,7 @@ const Home = () => {
             <Info></Info>
             <Services></Services>
             <ExeptionalService></ExeptionalService>
+            <HomeAppoinment></HomeAppoinment>
         </div>
     );
 };
--- src/Pages/Home/HomeAppoinment.js
+++ src/Pages/Home/HomeAppoinment.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import doctorImage from '../../assets/images/doctor.png';
+import appoinmentImage from '../../assets/images/appointment.png';
+import PrimaryButton from '../Shared/PrimaryButton';
 
 const HomeAppoinment = () => {
     return (
-        <div>
-
+        <div className='flex justify-center items-center'>
+            <div className='flex-1 w-[606px]'>
+                <img src={doctorImage} alt="" />
+            </div>
+            <div className='flex-1'>
+                <p className='text-primary font-bold my-2'>Appoinment</p>
+                <p className='my-2 text-[36px]'>Make an appointment Today</p>
+                <p className='my-6'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <PrimaryButton>Get STarted</PrimaryButton>
+            </div>
         </div>
     );
 };
