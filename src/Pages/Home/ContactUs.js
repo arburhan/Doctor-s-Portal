04e96d1e896a64019931531e654a81fedcb4cf86
arburhan <buthan190629@outlook.com>
2022-05-12 12:38:03 +0600

    style add on contact

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -16,7 +16,7 @@ const ContactUs = () => {
             <div className='flex flex-col gap-4 items-center my-8'>
                 <input type="email" placeholder="Email Address" class="input input-bordered w-full lg:max-w-md max-w-xs" />
                 <input type="text" placeholder="Subject" class="input input-bordered w-full lg:max-w-md max-w-xs" />
-                <input type="text" placeholder="Your message" class="input input-bordered py-24 w-full lg:max-w-md max-w-xs" />
+                <textarea type="text" placeholder="Your message" class="w-full p-4 max-w-md rounded-md" rows={6} />
                 <PrimaryButton customClass='px-12'> Submit </PrimaryButton>
             </div>
         </section>
