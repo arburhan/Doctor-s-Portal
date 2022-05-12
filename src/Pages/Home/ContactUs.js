cf10c9298389ed1b9795ec84a334575852264a1d
arburhan <buthan190629@outlook.com>
2022-05-12 12:23:44 +0600

    style add on contact section

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -8,15 +8,15 @@ const ContactUs = () => {
     return (
         <section style={{
             background: `url(${contactbg})`
-        }} className='my-24 text-center py-16'>
+        }} className='my-24 text-center py-16 px-6'>
             <div>
                 <p className='text-primary font-bold my-1'>Contact Us</p>
                 <h3 className='mb-2 text-[36px] text-white'>Stay connected with us</h3>
             </div>
             <div className='flex flex-col gap-4 items-center my-8'>
-                <input type="email" placeholder="Email Address" class="input input-bordered w-full max-w-xs" />
-                <input type="text" placeholder="Subject" class="input input-bordered w-full max-w-xs" />
-                <input type="text" placeholder="Your message" class="input input-bordered py-24 w-full max-w-xs" />
+                <input type="email" placeholder="Email Address" class="input input-bordered w-full lg:max-w-md max-w-xs" />
+                <input type="text" placeholder="Subject" class="input input-bordered w-full lg:max-w-md max-w-xs" />
+                <input type="text" placeholder="Your message" class="input input-bordered py-24 w-full lg:max-w-md max-w-xs" />
                 <PrimaryButton customClass='px-12'> Submit </PrimaryButton>
             </div>
         </section>
