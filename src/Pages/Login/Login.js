9476f65689f663db3ef67015b572a40c92dc475a
arburhan <buthan190629@outlook.com>
2022-05-13 21:22:44 +0600

    create google sign

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     return (
         <div className='flex h-screen items-center justify-center'>
             <div class="card w-96 bg-base-100 shadow-xl">
@@ -8,7 +11,9 @@ const Login = () => {
                     <h2 class="text-2xl font-bold text-center">Login</h2>
 
                     <div class="divider">OR</div>
-                    <button class="btn btn-outline">Continue with google</button>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline">Continue with google</button>
                 </div>
             </div>
         </div>
