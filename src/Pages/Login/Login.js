6797cbe448fe8e3dfb028d8236b030f192935dfd
arburhan <buthan190629@outlook.com>
2022-05-13 20:01:11 +0600

    customize login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
+        <div className='flex h-screen items-center justify-center'>
             <div class="card w-96 bg-base-100 shadow-xl">
                 <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                    <h2 class="text-2xl font-bold text-center">Login</h2>
+
+                    <div class="divider">OR</div>
+                    <button class="btn btn-outline">Continue with google</button>
                 </div>
             </div>
         </div>
