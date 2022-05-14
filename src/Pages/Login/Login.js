efcd9ef9e0e2157c75d53ebaadeccea8620d6343
arburhan <buthan190629@outlook.com>
2022-05-14 19:56:35 +0600

    install react from & input field added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div className='flex h-screen items-center justify-center'>
             <div class="card w-96 bg-base-100 shadow-xl">
                 <div class="card-body">
                     <h2 class="text-2xl font-bold text-center">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="Your Email" class="input input-bordered w-full max-w-xs" />
+                            <label class="label">
+                                <span class="label-text-alt">Alt label</span>
+                            </label>
+                        </div>
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="Your Password" class="input input-bordered w-full max-w-xs" />
+                            <label class="label">
+                                <span class="label-text-alt">Alt label</span>
+                            </label>
+                        </div>
+
+                        <input className='btn btn-active' type="submit" />
+                    </form>
 
                     <div class="divider">OR</div>
                     <button
