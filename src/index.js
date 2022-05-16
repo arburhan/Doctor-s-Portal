6ea9004deb73921e482aa235a2a80b38b10f5634
arburhan <buthan190629@outlook.com>
2022-05-16 09:55:43 +0600

    i react query

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+// Create a client
+const queryClient = new QueryClient();
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </QueryClientProvider>
+  ,
   document.getElementById('root')
 );
 
